2b72e6287f59da611672fd32cd3a444ebf238fab
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-17 16:22:29 +0100

    Add: Type Table, Seeder, Model and Foreign key

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['FrontEnd', 'BackEnd', 'FullStack', 'DevOps'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name);
+            $new_type->save();
+        }
+    }
+}
