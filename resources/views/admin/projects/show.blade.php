d3487cf049f66cbdd387f5f4f05bcb09534027e2
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-17 17:05:39 +0100

    Add: type to view show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container mt-3">
         <h1 class="text-center">{{ $project->title }}</h1>
+        <p class="text-center">{{ $project->type ? $project->type->name : 'No Type' }}</p>
         <div class="d-flex justify-content-between">
             <h5>{{ $project->created_at }}</h5>
             <p> {{ $project->slug }}</p>
